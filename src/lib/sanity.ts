095a959bd0c085ff0693595352affa9ce322e040
Natan Motta <natan.valladao@gmail.com>
2025-07-07 21:14:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,15 +2,15 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
-const projectId = process.env.SANITY_PROJECT_ID!;
-const dataset = process.env.SANITY_DATASET!;
+const projectId = process.env.SANITY_PROJECT_ID || 'replace-with-your-project-id';
+const dataset = process.env.SANITY_DATASET || 'production';
 const apiVersion = '2024-05-01';
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false, // `false` if you want to ensure fresh data for ISR
+  useCdn: false,
   token: process.env.SANITY_API_TOKEN,
 });
 
